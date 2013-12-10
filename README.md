81e1fee4bc772415a22772c2ee8f1a079c5090f5
Jesse Glick <jglick@cloudbees.com>
2013-12-10 13:17:47 -0500

    Adding clone.groovy.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 This script generates a single Git repository that contains all Git repositories in GitHub. This huge union repository is convenient for datamining and other analytics.
 
 `git clone http://git.jenkins-ci.org/all.git` to clone this repository. For each branch `foo` in the repository `http://github.com/jenkinsci/bar`, the union repository contains `bar/foo`. The same naming convention applies for tags.
+
+Also see `clone.groovy` if you prefer to check out separate repositories with working copies.
---
+++ clone.groovy
@@ -0,0 +1,30 @@
+#!/usr/bin/env groovy
+
+// As an alternative to all.git, just makes sure you have all repositories cloned. Useful for searching.
+// Clones into subdirectories of the current directory (so invoke as ./backend-merge-all-repo/clone.groovy).
+
+@Grab("org.kohsuke:github-api:1.47")
+import org.kohsuke.github.*;
+
+GitHub gh;
+if (new File(System.getProperty("user.home"), ".github").file) {
+    println('connecting as per ~/.github');
+    gh = GitHub.connect();
+} else {
+    println('connecting anonymously; rate limiting will probably make it impossible to do this twice in a row');
+    println('try: echo oauth=… > ~/.github');
+    gh = GitHub.connectAnonymously();
+}
+
+for (repo in gh.getOrganization("jenkinsci").listRepositories()) {
+    if (new File(repo.name).directory) {
+        println("${repo.name} already here, skipping");
+    } else { 
+        println("cloning ${repo.name} with size ${repo.size}");
+        def r = new ProcessBuilder('git', 'clone', "git@github.com:jenkinsci/${repo.name}.git").redirectOutput(ProcessBuilder.Redirect.INHERIT).start().waitFor();
+        if (r != 0) {
+            println('Failed!');
+            System.exit(1);
+        }
+    }
+}
